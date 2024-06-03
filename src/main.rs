122ae34e33c96ce3215e57f97612975ca22196e9
madomado <madonuko@outlook.com>
2024-06-04 00:58:59 +0800

    feat: kinda finish home page?

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,56 @@ pub mod entities;
 
 pub use entities::prelude as orm;
 use rocket::{
-    http::{Cookie, CookieJar},
+    http::{Cookie, CookieJar, Status},
     response::Redirect,
 };
 use rocket_dyn_templates::Template;
 use rocket_oauth2::OAuth2;
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QuerySelect};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[rocket::get("/")]
-fn index(user: db::User, db: &rocket::State<sea_orm::DatabaseConnection>) -> Template {
-    Template::render(
+async fn index(
+    user: db::User,
+    db: &rocket::State<sea_orm::DatabaseConnection>,
+) -> Result<Template, Status> {
+    let db = db as &sea_orm::DatabaseConnection;
+    let hevents = orm::HoldEvent::find()
+        .filter(entities::hold_event::Column::Admin.eq(user.id.clone()))
+        .limit(10)
+        .all(db)
+        .await
+        .expect("find all user holdevents");
+    let mut new_hevents = vec![];
+    for hevent in hevents {
+        let Ok(Some(hevent)) = orm::Event::find_by_id(hevent.event).one(db).await else {
+            tracing::error!(?hevent, "Cannot get HoldEvent");
+            return Err(Status::InternalServerError);
+        };
+        new_hevents.push(hevent);
+    }
+    let jevents = orm::JoinEvent::find()
+        .filter(entities::join_event::Column::User.eq(user.id.clone()))
+        .limit(10)
+        .all(db)
+        .await
+        .expect("find all user joinevents");
+    let mut new_jevents = vec![];
+    for jevent in jevents {
+        let Ok(Some(jevent)) = orm::Event::find_by_id(jevent.event).one(db).await else {
+            tracing::error!(?jevent, "Cannot get JoinEvent");
+            return Err(Status::InternalServerError);
+        };
+        new_jevents.push(jevent);
+    }
+    Ok(Template::render(
         "index",
         rocket_dyn_templates::context! {
-            user
+            user,
+            hevents: new_hevents,
+            jevents: new_jevents,
         },
-    )
+    ))
 }
 
 #[rocket::get("/", rank = 2)]
