c8344e6e250c6ae077c1c5e8eb832d9a4b2dd347
madomado <madonuko@outlook.com>
2024-06-11 10:51:07 +0800

    feat: wip subevents

--- src/pages/subevents.rs
+++ src/pages/subevents.rs
@@ -6,7 +6,7 @@ use crate::{db, entities, orm};
 
 use super::events::get_evt;
 
-pub fn ruotes() -> impl Into<Vec<rocket::Route>> {
+pub fn routes() -> impl Into<Vec<rocket::Route>> {
     rocket::routes![get]
 }
 
@@ -18,9 +18,12 @@ pub async fn get(
     db: &db::DbConnGuard,
 ) -> Result<Template, (Status, Redirect)> {
     let db = db as &db::DbConn;
-    let managed_events =
-        orm::HoldEvent::find().filter(entities::hold_event::Column::Admin.eq(user.id));
-    let thisevent = managed_events.filter(entities::hold_event::Column::Event.eq(evtid));
+    let hevt = orm::HoldEvent::find()
+        .filter(entities::hold_event::Column::Admin.eq(user.id))
+        .filter(entities::hold_event::Column::Event.eq(evtid));
+    let jevt = orm::JoinEvent::find()
+        .filter(entities::join_event::Column::User.eq(user.id))
+        .filter(entities::join_event::Column::Event.eq(evtid));
     let event = get_evt(evtid, db)
         .await
         .expect("event in hevents not exist");
@@ -34,13 +37,31 @@ pub async fn get(
             Redirect::to(rocket::uri!(super::events::get(evtid))),
         ));
     };
+    let state = if let Some(_) = hevt.one(db).await.expect("can't select holdevents") {
+        "admin"
+    } else if let Some(_) = jevt.one(db).await.expect("can't select joinevents") {
+        "join"
+    } else {
+        return Err((Status::Forbidden, Redirect::to(rocket::uri!("/"))));
+    };
     Ok(Template::render(
         "subevents",
-        context! {
-            event,
-            subevt,
-            state: if thisevent.one(db).await.expect("can't select holdevents").is_some() { "admin" } else if             orm::JoinEvent::find().filter(entities::join_event::Column::User.eq(user.id)).filter(entities::join_event::Column::Event.eq(evtid)).one(db).await.expect("can't select joinevents").is_some()
-            { "join" } else { return Err((Status::Forbidden, Redirect::to(rocket::uri!("/")))) }
-        },
+        context! { event, subevt, state },
     ))
+}
+#[rocket::get("/<evtid>/subevts/user/<uid>")]
+async fn get_user(
+    user: db::User,
+    evtid: i32,
+    uid: i32,
+    db: &db::DbConnGuard,
+) -> Result<Template, (Status, Redirect)> {
+    let db = db as &db::DbConn;
+    let hevt = orm::HoldEvent::find()
+        .filter(entities::hold_event::Column::Admin.eq(user.id))
+        .filter(entities::hold_event::Column::Event.eq(evtid));
+    if let None = hevt.one(db).await.expect("can't select holdevents") {
+        return Err((Status::Forbidden, Redirect::to(rocket::uri!("/"))));
+    }
+    todo!()
 }
